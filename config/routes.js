1469af0899fee3ba2ddaa741eed94bd9e2534bd7
focused220 <roblhjr.220@gmail.com>
2019-06-14 18:56:12 -0700

    initial commit

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
-
 const { authenticate } = require('../auth/authenticate');
+const bcrypt = require('bcryptjs');
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+const DB = knex(knexConfig.development);
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +13,38 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  DB.insert(user)
+    .then(saved => {
+      const token = tokenService.generateToken(saved);
+      res.status(201).json({ token });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+
+  const user = await DB('users').where('username', '=', { username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 }
 
 function getJokes(req, res) {
